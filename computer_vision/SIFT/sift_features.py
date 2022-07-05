6cde2539295f95158155f6bffd19b5f3bbee0d2a
kvnptl <kevinpatel4400@gmail.com>
2022-07-05 15:56:50 +0200

    udpate: SIFT feature matching

--- computer_vision/SIFT/sift_features.py
+++ computer_vision/SIFT/sift_features.py
@@ -7,29 +7,40 @@ Topic: SIFT is a feature detector and descriptor. It is used to find the feature
 
 '''
 
-import cv2 
-import matplotlib.pyplot as plt
-# %matplotlib inline
-
-#reading image
-img1 = cv2.imread('eiffel_1.jpeg')  
-gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-
-#keypoints
-sift = cv2.xfeatures2d.SIFT_create()
-keypoints_1, descriptors_1 = sift.detectAndCompute(img1,None)
-
-img_1 = cv2.drawKeypoints(gray1,keypoints_1,img1)
-plt.imshow(img_1)
+import cv2
 
 # read images
-img1 = cv2.imread('eiffel_2.jpeg')  
-img2 = cv2.imread('eiffel_1.jpg') 
+img1 = cv2.imread('computer_vision/SIFT/eiffel_1.jpg')
+img2 = cv2.imread('computer_vision/SIFT/eiffel_2.jpg')
 
+# convert to gray scale
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-figure, ax = plt.subplots(1, 2, figsize=(16, 8))
+# sift
+sift = cv2.SIFT_create()
 
-ax[0].imshow(img1, cmap='gray')
-ax[1].imshow(img2, cmap='gray')
+# find the keypoints and descriptors with SIFT
+keypoints_1, descriptors_1 = sift.detectAndCompute(img1, None)
+keypoints_2, descriptors_2 = sift.detectAndCompute(img2, None)
+
+print("Total keypoints: Image 1: {}, Image 2: {}".format(
+    len(keypoints_1), len(keypoints_2)))
+
+# feature matching
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+matches = bf.match(descriptors_1, descriptors_2)
+matches = sorted(matches, key=lambda x: x.distance)
+
+print("Number of matched features: ", len(matches))
+
+img3 = cv2.drawMatches(img1, keypoints_1, img2,
+                       keypoints_2, matches[:50], img2, flags=2)
+
+# show image
+# cv2.imshow('image1', img1)
+# cv2.imshow('image2', img2)
+cv2.imshow('image3', img3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
